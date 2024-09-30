14da4467e94d0756702685ab800d4cc393e84f6a
adhvyktech <adhvyktech@gmail.com>
2024-09-30 22:55:59 +0530

    Implement AR experience generation, QR code, and viewing

--- src/custom-elements.d.ts
+++ src/custom-elements.d.ts
@@ -1,5 +1,5 @@
 declare namespace JSX {
     interface IntrinsicElements {
-      'a-image': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-image': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { src: string }, HTMLElement>;
     }
   }
