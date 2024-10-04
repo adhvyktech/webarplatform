aa6bc118cdee68f2b191babc660b070bacc06236
adhvyktech <adhvyktech@gmail.com>
2024-10-04 08:40:30 +0530

    Implement robust error handling and AR experience viewing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  // ... other config
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-  },
-};
+    extends: ['react-app', 'react-app/jest'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+    },
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+  };
